f735541d9d8f76c65c2902f1c41f0141c1744fe1
Nora Harris <noragharris@gmail.com>
2021-12-03 15:38:13 -0600

    Switch to postgresql from sqlite3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
     it "test testing" do
-        expect(true).to be(true)
+        get :index
+        expect(response).to have_http_status(200)
     end
 end
